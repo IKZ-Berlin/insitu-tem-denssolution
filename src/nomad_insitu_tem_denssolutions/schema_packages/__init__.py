38c17910980ebfa46a009db26f04762fd1fa1c80
Sebastian Brueckner <sebasdaszebra@gmail.com>
2024-08-20 09:51:54 +0200

    read csv and log

--- src/nomad_insitu_tem_denssolutions/schema_packages/__init__.py
+++ src/nomad_insitu_tem_denssolutions/schema_packages/__init__.py
@@ -7,11 +7,28 @@ class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
         from nomad_insitu_tem_denssolutions.schema_packages.mypackage import m_package
- 
+
         return m_package
 
 
 mypackage = MySchemaPackageEntryPoint(
     name='MyPackage',
+    description='Schema package defined using the new plugin mechanism.',
+)
+
+
+class DensSolutionsPackageEntryPoint(SchemaPackageEntryPoint):
+    # parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from nomad_insitu_tem_denssolutions.schema_packages.denssolutions import (
+            m_package,
+        )
+
+        return m_package
+
+
+denssolutions = DensSolutionsPackageEntryPoint(
+    name='DensSolutions',
     description='Schema package defined using the new plugin mechanism.',
 )
